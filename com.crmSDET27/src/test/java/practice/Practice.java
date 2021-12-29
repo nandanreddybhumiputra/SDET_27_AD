88a38516dde2b2e455c0f0e6afb895b826f8b9c7
Hello World <Hello World@DESKTOP-LELF0B0>
2021-12-29 17:50:31 +0530

    adding m1 method in practice_practice class

--- com.crmSDET27/src/test/java/practice/Practice.java
+++ com.crmSDET27/src/test/java/practice/Practice.java
@@ -6,9 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Practice {
 	public static void main(String[] args) {
 		
-		WebDriver driver=new ChromeDriver();
+		System.out.println("hi");
 		
 		
 	}
 
+	public void m1() {
+		
+		System.out.println("m1");
+	}
+	
+	
+	
 }
